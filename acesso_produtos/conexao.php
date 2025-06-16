8e817ec2d39259ae8438330c0765fb28096e7dfb
IsaqueMarley <116613395+IsaqueMarley@users.noreply.github.com>
2025-06-16 19:22:17 -0300

    Update conexao.php

--- acesso_produtos/conexao.php
+++ acesso_produtos/conexao.php
@@ -1,16 +1,1 @@
-<?php
-
-$dbHost = '2cc149d1d993d1db32fd1403265c';
-$dbUsername = 'sequencia';
-$dbPassword = 'WMefHmcbkurh97GoxR';
-$dbName = 'foSuSotxeB8WkCkGgZztG9';
-
-
-try {
-    $conexao = new PDO("mysql:host=$dbHost;dbname=$dbName", $dbUsername, $dbPassword);
-    $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Erro na conexão com o banco de dados: " . $e->getMessage());
-}
-
-?>
+.
